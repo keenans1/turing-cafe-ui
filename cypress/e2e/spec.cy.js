cd7fd79f47ac215595e08eb76ef03e45b52b7d6d
Keenan Southall <keenan.southall@yahoo.com>
2023-02-21 11:31:22 -0700

    Implement tests according to spec

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,36 @@
 describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+  beforeEach(() => {
+    cy.intercept("GET", "http://localhost:3001/api/v1/reservations", [
+      {
+        id: 3,
+        name: "Pam",
+        date: "1/21",
+        time: "6:00",
+        number: 4
+      }
+    ])
+    cy.visit('http://localhost:3000/')
+  })
+  it('should display the website title', () => {
+    cy.get('h1').contains("Turing Cafe Reservations")
+  })
+  it('should display reservations', () => {
+    cy.get('div[class="resy-container"]')
+      .should('have.class', 'resy-container')
+  })
+  it('should type an input', () => {
+    cy.get('form').within(() => {
+      cy.get('input[name="name"]').type('abc')
+    })
+  })
+  it('should enter info, click add button, and see new reservation', () => {
+    cy.get('form').within(() => {
+      cy.get('input[name="name"]').type('xyz')
+        .get('input[name="date"]').type('8/4')
+        .get('input[name="time"]').type('6:15')
+        .get('input[name="number"]').type('5')
+        .get('button').click()
+    })
+    cy.contains('xyz')
   })
 })
